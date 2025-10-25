3e39a2c77d01740dc89f34dea50ded5a33398db5
necopagas <80867830+necopagas@users.noreply.github.com>
2025-10-25 22:42:49 +0800

    Refactor config.js for API and IPTV channels
    Removed unused API paths and updated IPTV channels structure.

--- src/config.js
+++ src/config.js
@@ -4,8 +4,6 @@
 export const API_KEY = "3b0f392b6173455e37624a78bd5f79d4";
 export const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 export const BACKDROP_PATH = "https://image.tmdb.org/t/p/original";
-export const POSTER_SMALL = "https://image.tmdb.org/t/p/w342";
-export const PROFILE_PATH = "https://image.tmdb.org/t/p/w185";
 
 // Keywords
 const ANIME_KEYWORD = "210024";
@@ -19,12 +17,7 @@ export const ANIME_GENRES = [
   { id: 18, name: "Drama" },
   { id: 10765, name: "Sci-Fi & Fantasy" },
   { id: 9648, name: "Mystery" },
-  { id: 10762, name: "Kids" },
-  { id: 10764, name: "Reality" },
-  { id: 10766, name: "Soap" },
-  { id: 10767, name: "Talk" },
-  { id: 10768, name: "War & Politics" },
-  { id: 99999, name: "Isekai" },
+  { id: 99999, name: "Isekai" }
 ];
 
 export const MOVIE_GENRES = [
@@ -33,7 +26,7 @@ export const MOVIE_GENRES = [
   { id: 18, name: "Drama" }, { id: 10751, name: "Family" }, { id: 14, name: "Fantasy" },
   { id: 36, name: "History" }, { id: 27, name: "Horror" }, { id: 10402, name: "Music" },
   { id: 9648, name: "Mystery" }, { id: 10749, name: "Romance" }, { id: 878, name: "Science Fiction" },
-  { id: 10770, name: "TV Movie" }, { id: 53, name: "Thriller" }, { id: 10752, name: "War" },
+  { id: 10770, name: "TV Movie" }, { id: 53, name: "Thriller" }, { id: -man 10752, name: "War" },
   { id: 37, name: "Western" }
 ];
 
@@ -42,186 +35,147 @@ const tmdb = (path) => `https://api.themoviedb.org/3${path}?api_key=${API_KEY}`;
 
 export const API_ENDPOINTS = {
   trending: tmdb("/trending/all/day"),
-  popularMovies: tmdb("/movie/popular"),
-  topRatedMovies: tmdb("/movie/top_rated"),
-  popularTV: tmdb("/tv/popular"),
-  tvAiringToday: tmdb("/tv/airing_today"),
-  asianDramas: `${tmdb("/discover/tv")}&with_genres=18&with_origin_country=KR,JP,CN,TH,TW,HK&sort_by=popularity.desc`,
+  popular: tmdb("/movie/popular"),
+  toprated: tmdb("/movie/top_rated"),
+  tvshows: tmdb("/tv/popular"),
+  asianDramas: `${tmdb("/discover/tv")}&with_genres=18&with_origin_country=KR,JP,CN,TH&sort_by=popularity.desc`,
   
   anime: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD}&sort_by=popularity.desc`,
   animePopular: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD}&sort_by=popularity.desc`,
-  animeTopRated: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD}&sort_by=vote_average.desc&vote_count.gte=300`,
+  animeTopRated: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD}&sort_by=vote_average.desc&vote_count.gte=500`,
   animeNewReleases: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD}&sort_by=first_air_date.desc&air_date.lte=${CURRENT_DATE}`,
   animeByGenre: (genreId) => `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD}&with_genres=${genreId}&sort_by=popularity.desc`,
-  animeIsekai: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD},${ISEKAI_KEYWORD}&sort_by=popularity.desc`, // FIXED
+  animeIsekai: `${tmdb("/discover/tv")}&with_keywords=${ANIME_KEYWORD},${ISEKAI_KEYWORD}&sort_by=popularity.desc`,
 
-  search: (query, page = 1) => `${tmdb("/search/multi")}&query=${encodeURIComponent(query)}&page=${page}&include_adult=false`,
-  details: (type, id) => `${tmdb(`/${type}/${id}`)}&append_to_response=videos,credits,external_ids,images,release_dates`,
+  search: (query, page = 1) => `${tmdb("/search/multi")}&query=${encodeURIComponent(query)}&page=${page}`,
+  details: (type, id) => `${tmdb(`/${type}/${id}`)}&append_to_response=videos,credits,external_ids`,
   recommendations: (type, id) => tmdb(`/${type}/${id}/recommendations`),
-  byGenre: (genreId, type = "movie") => `${tmdb(`/discover/${type}`)}&with_genres=${genreId}&sort_by=popularity.desc`,
+  byGenre: (genreId) => `${tmdb("/discover/movie")}&with_genres=${genreId}&sort_by=popularity.desc`,
 };
 
-// --- EMBED SOURCES (2025 WORKING) ---
+// --- ORIGINAL EMBED SOURCES (TANGGALON ANG BAG-O) ---
 export const EMBED_URLS = {
-  superembed: {
-    movie: (id) => `https://www.superembed.stream/embed/movie/${id}`,
-    tv: (id, s, e) => `https://www.superembed.stream/embed/tv/${id}/${s}/${e}`
-  },
-  '2embed': {
-    movie: (id) => `https://www.2embed.cc/embed/${id}`,
-    tv: (id, s, e) => `https://www.2embed.cc/embedtv/${id}&s=${s}&e=${e}`
-  },
-  vidsrc_me: {
-    movie: (id) => `https://vidsrc.me/embed/movie/${id}`, // FIXED: E(id) → (id)
-    tv: (id, s, e) => `https://vidsrc.me/embed/tv/${id}/${s}/${e}`
-  },
   vidsrc: {
     movie: (id) => `https://vidsrc.to/embed/movie/${id}`,
     tv: (id, s, e) => `https://vidsrc.to/embed/tv/${id}/${s}/${e}`
-  },
-  vidsrc_cc: {
-    movie: (id) => `https://vidsrc.cc/v2/embed/${id}`,
-    tv: (id, s, e) => `https://vidsrc.cc/v2/embed/${id}/${s}/${e}`
-  },
-  vidsrc_in: {
-    movie: (id) => `https://vidsrc.in/embed/movie/${id}`,
-    tv: (id, s, e) => `https://vidsrc.in/embed/tv/${id}/${s}/${e}`
-  },
-  flixhq: {
-    movie: (id) => `https://flixhq.to/movie/${id}`,
-    tv: (id, s, e) => `https://flixhq.to/tv/${id}/season-${s}-episode-${e}`
-  },
-  smashystream: {
-    movie: (id) => `https://embed.smashystream.com/playitagaintom.php?tmdb=${id}`,
-    tv: (id, s, e) => `https://embed.smashystream.com/playitagaintom.php?tmdb=${id}&season=${s}&episode=${e}`
-  },
-  autoembed: {
-    movie: (id) => `https://autoembed.pro/embed/movie/${id}`,
-    tv: (id, s, e) => `https://autoembed.pro/embed/tv/${id}/${s}/${e}`
-  },
-  vidlink: {
-    movie: (id) => `https://vidlink.pro/movie/${id}`,
-    tv: (id, s, e) => `https://vidlink.pro/tv/${id}/${s}/${e}`
-  },
-  vidfast: {
-    movie: (id) => `https://vidfast.pro/movie/${id}?autoPlay=true`,
-    tv: (id, s, e) => `https://vidfast.pro/tv/${id}/${s}/${e}?autoPlay=true`
   },
   multiembed: {
     movie: (id) => `https://multiembed.mov/?video_id=${id}&tmdb=1`,
     tv: (id, s, e) => `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${s}&e=${e}`
   },
+  vidlink: {
+    movie: (id) => `https://vidlink.pro/movie/${id}`,
+    tv: (id, s, e) => `https://vidlink.pro/tv/${id}/${s}/${e}`
+  },
   videasy: {
     movie: (imdb_id) => imdb_id ? `https://player.videasy.net/movie/${imdb_id}` : null,
     tv: (imdb_id, s, e) => imdb_id ? `https://player.videasy.net/tv/${imdb_id}/${s}/${e}` : null
   },
-  animepahe: {
-    anime: (mal_id, episode = 1) => `https://animepahe.ru/play/${mal_id}/${episode}`
+  vidfast: {
+    movie: (id) => `https://vidfast.pro/movie/${id}?autoPlay=true`,
+    tv: (id, s, e) => `https://vidfast.pro/tv/${id}/${s}/${e}?autoPlay=true`
   },
-  gogoanime: {
-    anime: (mal_id, episode = 1, dub = false) => {
-      const type = dub ? '-dub' : '';
-      return `https://gogoanime3.net/play/${mal_id}${type}-episode-${episode}`;
-    }
+  autoembed: {
+    movie: (id) => `https://autoembed.pro/embed/movie/${id}`,
+    tv: (id, s, e) => `https://autoembed.pro/embed/tv/${id}/${s}/${e}`
   },
   vidlink_anime: {
-    anime: (mal_id, episode = 1, dub = "sub") => `https://vidlink.pro/anime/${mal_id}/${episode}/${dub}?fallback=true`
+    anime: (mal_id, e, type) => `https://vidlink.pro/anime/${mal_id}/${e}/${type}?fallback=true`
   }
 };
 
-// --- SOURCE ORDER ---
-export const SOURCE_ORDER = [
-  'superembed', '2embed', 'vidsrc_me',
-  'vidsrc', 'vidsrc_cc', 'vidsrc_in',
-  'flixhq', 'smashystream',
-  'autoembed', 'vidlink', 'vidfast', 'multiembed', 'videasy',
-  'animepahe', 'gogoanime', 'vidlink_anime'
-];
+// --- ORIGINAL SOURCE ORDER ---
+export const SOURCE_ORDER = ['autoembed', 'vidlink', 'vidfast', 'vidsrc', 'multiembed', 'videasy'];
 
 // --- PROXY ---
-const proxyCache = new Map();
-export const proxy = (url) => {
-  if (proxyCache.has(url)) return proxyCache.get(url);
-  const proxied = `https://corsproxy.io/?${encodeURIComponent(url)}`;
-  proxyCache.set(url, proxied);
-  return proxied;
-};
+const proxy = (url) => `https://corsproxy.io/?${encodeURIComponent(url)}`;
 
 // --- IPTV CHANNELS ---
-export const IPTV_CHANNELS = {
-  pinoy: [
-    { name: "GMA 7", url: proxy("https://amg01006-abs-cbn-abscbn-gma-x7-dash-abscbnono-dzsx9.amagi.tv/index.mpd") },
-    { name: "JUNGO TV PINOY", url: proxy("https://jungotvstream.chanall.tv/jungotv/jungopinoytv/stream.m3u8") },
-    { name: "HALLYPOP", url: proxy("https://jungotvstream.chanall.tv/jungotv/hallypop/stream.m3u8") },
-  ],
-  anime: [
-    { name: "ANIME X HIDIVE", url: proxy("https://amc-anime-x-hidive-1-us.tablo.wurl.tv/playlist.m3u8") },
-  ],
-  kids: [
-    { name: "CARTOON NETWORK", url: proxy("https://cdn3.skygo.mn/live/disk1/Cartoon_Network/HLSv3-FTA/Cartoon_Network.m3u8") },
-    { name: "DISNEY CHANNEL", url: proxy("https://fl5.moveonjoy.com/DISNEY_CHANNEL/index.m3u8") },
-    { name: "NICK JR", url: proxy("https://fl5.moveonjoy.com/NICK_JR/index.m3u8") },
-  ],
-  movies: [
-    { name: "HBO", url: proxy("https://fl2.moveonjoy.com/HBO/index.m3u8") },
-    { name: "SHOWTIME", url: proxy("https://fl2.moveonjoy.com/SHOWTIME/index.m3u8") },
-  ],
-  discovery: [
-    { name: "DISCOVERY CHANNEL", url: proxy("https://fl3.moveonjoy.com/Discovery_Channel/index.m3u8") },
-    { name: "ANIMAL PLANET", url: proxy("https://fl5.moveonjoy.com/Animal_Planet/index.m3u8") },
-  ],
-  sports: [
-    { name: "NBA TV", url: proxy("https://fl5.moveonjoy.com/NBA_TV/index.m3u8") },
-  ]
-};
-export const IPTV_CHANNELS_FLAT = Object.values(IPTV_CHANNELS).flat();
+export const IPTV_CHANNELS = [
+  { name: 'GMA 7', url: proxy('https://amg01006-abs-cbn-abscbn-gma-x7-dash-abscbnono-dzsx9.amagi.tv/index.mpd') },
+  { name: 'JUNGO TV PINOY', url: proxy('https://jungotvstream.chanall.tv/jungotv/jungopinoytv/stream.m3u8') },
+  { name: 'SCREAMFLIX', url: proxy('https://jungotvstream.chanall.tv/jungotv/screamflix/stream.m3u8') },
+  { name: 'HALLYPOP', url: proxy('https://jungotvstream.chanall.tv/jungotv/hallypop/stream.m3u8') },
+  { name: 'FRONTROW', url: proxy('https://jungotvstream.chanall.tv/jungotv/frontrow/stream.m3u8') },
+  { name: 'COMBATGO', url: proxy('https://jungotvstream.chanall.tv/jungotv/combatgo/stream.m3u8') },
+  { name: 'AWSN', url: proxy('https://amg02188-amg02188c2-jungotv-northamerica-5717.playouts.now.amagi.tv/playlist.m3u8') },
+  { name: 'Hi-YAH!', url: proxy('https://linear-59.frequency.stream/dist/roku/59/hls/master/playlist.m3u8') },
+  { name: 'ANIME X HIDIVE', url: proxy('https://amc-anime-x-hidive-1-us.tablo.wurl.tv/playlist.m3u8') },
+  { name: 'LOTUS MACAU', url: proxy('https://cdn3.skygo.mn/live/disk1/Lotus/HLSv3-FTA/Lotus.m3u8') },
+  { name: 'TRAVELXP', url: proxy('https://travelxp-travelxp-1-eu.rakuten.wurl.tv/playlist.m3u8') },
+  { name: 'DISCOVERY ASIA', url: proxy('https://cdn3.skygo.mn/live/disk1/Discovery_Asia/HLSv3-FTA/Discovery_Asia.m3u8') },
+  { name: 'DISCOVERY CHANNEL', url: proxy('https://fl3.moveonjoy.com/Discovery_Channel/index.m3u8') },
+  { name: 'NBA TV', url: proxy('https://fl5.moveonjoy.com/NBA_TV/index.m3u8') },
+  { name: 'NHL NETWORK', url: proxy('https://fl6.moveonjoy.com/NHL_NETWORK/index.m3u8') },
+  { name: 'ANIMAL PLANET', url: proxy('https://fl5.moveonjoy.com/Animal_Planet/index.m3u8') },
+  { name: 'CARTOONITO', url: proxy('https://cdn3.skygo.mn/live/disk1/Boomerang/HLSv3-FTA/Boomerang.m3u8') },
+  { name: 'CARTOON NETWORK', url: proxy('https://cdn3.skygo.mn/live/disk1/Cartoon_Network/HLSv3-FTA/Cartoon_Network.m3u8') },
+  { name: 'BABYSHARK TV', url: proxy('https://newidco-babysharktv-1-eu.rakuten.wurl.tv/playlist.m3u8') },
+  { name: 'KIDOODLE TV', url: proxy('https://kidoodletv-kdtv-1-us.samsung.wurl.tv/playlist.m3u8') },
+  { name: 'PBS KIDS', url: proxy('https://2-fss-2.streamhoster.com/pl_140/amlst:200914-129820/playlist.m3u8') },
+  { name: 'BBC KIDS', url: proxy('https://dmr1h4skdal9h.cloudfront.net/v1/manifest/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-msbj6srh7nhug/ce36558b-b031-4284-94be-91ed22d31a41/2.m3u8') },
+  { name: 'ZOOMOO', url: proxy('https://cdn3.skygo.mn/live/disk1/Zoomoo/HLSv3-FTA/Zoomoo.m3u8') },
+  { name: 'DISCOVERY FAMILY CHANNEL', url: proxy('https://fl5.moveonjoy.com/DISCOVERY_FAMILY_CHANNEL/index.m3u8') },
+  { name: 'DISNEY CHANNEL', url: proxy('https://fl5.moveonjoy.com/DISNEY_CHANNEL/index.m3u8') },
+  { name: 'DISNEY XD', url: proxy('https://fl5.moveonjoy.com/DISNEY_XD/index.m3u8') },
+  { name: 'DISNEY JR', url: proxy('https://fl2.moveonjoy.com/DISNEY_JR/index.m3u8') },
+  { name: 'NICKELODEON', url: proxy('https://fl1.moveonjoy.com/NICKELODEON/index.m3u8') },
+  { name: 'NICK JR', url: proxy('https://fl5.moveonjoy.com/NICK_JR/index.m3u8') },
+  { name: 'NICKTOONS', url: proxy('https://fl1.moveonjoy.com/NICKTOONS/index.m3u8') },
+  { name: 'LIFETIME', url: proxy('https://fl5.moveonjoy.com/LIFETIME/index.m3u8') },
+  { name: 'AMC NETWORK', url: proxy('https://fl2.moveonjoy.com/AMC_NETWORK/index.m3u8') },
+  { name: 'HALLMARK CHANNEL', url: proxy('https://fl2.moveonjoy.com/HALLMARK_CHANNEL/index.m3u8') },
+  { name: 'HALLMARK MOVIES MYSTERIES', url: proxy('https://fl2.moveonjoy.com/HALLMARK_MOVIES_MYSTERIES/index.m3u8') },
+  { name: 'HBO', url: proxy('https://fl2.moveonjoy.com/HBO/index.m3u8') },
+  { name: 'HBO 2', url: proxy('https://fl5.moveonjoy.com/HBO_2/index.m3u8') },
+  { name: 'HBO COMEDY', url: proxy('https://fl5.moveonjoy.com/HBO_COMEDY/index.m3u8') },
+  { name: 'HBO FAMILY', url: proxy('https://fl5.moveonjoy.com/HBO_FAMILY/index.m3u8') },
+  { name: 'HBO ZONE', url: proxy('https://fl5.moveonjoy.com/HBO_ZONE/index.m3u8') },
+  { name: 'SHOWTIME', url: proxy('https://fl2.moveonjoy.com/SHOWTIME/index.m3u8') },
+  { name: 'SHOWTIME 2', url: proxy('https://fl5.moveonjoy.com/SHOWTIME_2/index.m3u8') },
+  { name: 'SHOWTIME EXTREME', url: proxy('https://fl5.moveonjoy.com/SHOWTIME_EXTREME/index.m3u8') },
+  { name: 'SHOWTIME NEXT', url: proxy('https://fl5.moveonjoy.com/SHOWTIME_NEXT/index.m3u8') },
+  { name: 'SHOWTIME WOMEN', url: proxy('https://fl5.moveonjoy.com/SHOWTIME_WOMEN/index.m3u8') },
+  { name: 'STARZ', url: proxy('https://fl5.moveonjoy.com/STARZ/index.m3u8') },
+  { name: 'STARZ COMEDY', url: proxy('https://fl5.moveonjoy.com/STARZ_COMEDY/index.m3u8') },
+  { name: 'STARZ EDGE', url: proxy('https://fl5.moveonjoy.com/STARZ_EDGE/index.m3u8') },
+  { name: 'STARZ ENCORE', url: proxy('https://fl5.moveonjoy.com/STARZ_ENCORE/index.m3u8') },
+  { name: 'STARZ ENCORE ACTION', url: proxy('https://fl5.moveonjoy.com/STARZ_ENCORE_ACTION/index.m3u8') },
+  { name: 'STARZ ENCORE WESTERNS', url: proxy('https://fl5.moveonjoy.com/STARZ_ENCORE_WESTERNS/index.m3u8') },
+  { name: 'STARZ WEST', url: proxy('https://fl5.moveonjoy.com/STARZ_WEST/index.m3u8') },
+  { name: 'PARAMOUNT NETWORK', url: proxy('https://fl2.moveonjoy.com/PARAMOUNT_NETWORK/index.m3u8') },
+  { name: 'NICK MUSIC', url: proxy('https://fl5.moveonjoy.com/NICK_MUSIC/index.m3u8') },
+  { name: 'MTV', url: proxy('https://fl2.moveonjoy.com/MTV/index.m3u8') },
+  { name: 'MTV LIVE', url: proxy('https://fl2.moveonjoy.com/MTV_LIVE/index.m3u8') },
+  { name: 'NAT GEO WILD', url: proxy('https://fl5.moveonjoy.com/Nat_Geo_Wild/index.m3u8') },
+  { name: 'NATIONAL GEOGRAPHIC', url: proxy('https://fl5.moveonjoy.com/National_Geographic/index.m3u8') },
+  { name: 'HGTV', url: proxy('https://fl2.moveonjoy.com/HGTV/index.m3u8') },
+  { name: 'COMEDY CENTRAL', url: proxy('https://fl3.moveonjoy.com/Comedy_Central/index.m3u8') }
+];
 
 // --- ADMIN & COLLECTIONS ---
 export const ADMIN_UIDS = ['YOUR_FIREBASE_UID_HERE'];
 
 export const CURATED_COLLECTIONS = [
-  { title: "Halloween Horrors", ids: [{ id: 760161, type: "movie" }, { id: 965876, type: "movie" }, { id: 646385, type: "movie" }] },
-  { title: "Mind-Bending Sci-Fi", ids: [{ id: 603, type: "movie" }, { id: 157336, type: "movie" }, { id: 78, type: "movie" }] },
-  { title: "Top Anime", ids: [{ id: 94605, type: "tv" }, { id: 110427, type: "tv" }, { id: 40071, type: "tv" }] }
+  { title: "Halloween Horrors", ids: [ { id: 760161, type: 'movie' }, { id: 965876, type: 'movie' } ] },
+  { title: "Mind-Bending Sci-Fi", ids: [ { id: 603, type: 'movie' }, { id: 157336, type: 'movie' } ] },
 ];
 
-// === AUTO SERVER SWITCH ===
+// --- SIMPLE GET EMBED URL (NO AUTO SWITCH) ---
 export const getEmbedUrl = (type, id, season, episode, imdb_id, mal_id) => {
-  const isAnime = type === 'anime';
-  const isMovie = type === 'movie';
-  const isTV = type === 'tv';
-
-  const sources = [];
-
-  for (const sourceName of SOURCE_ORDER) {
-    const src = EMBED_URLS[sourceName];
+  for (const source of SOURCE_ORDER) {
+    const src = EMBED_URLS[source];
     if (!src) continue;
 
-    let url = null;
-
-    if (isAnime && mal_id && src.anime) {
-      if (sourceName === 'gogoanime') {
-        url = src.anime(mal_id, episode || 1, false);
-      } else if (sourceName === 'vidlink_anime') {
-        url = src.anime(mal_id, episode || 1, 'sub');
-      } else {
-        url = src.anime(mal_id, episode || 1);
-      }
-    } else if (isMovie && src.movie) {
-      url = sourceName === 'videasy' && imdb_id ? src.movie(imdb_id) : src.movie(id);
-    } else if (isTV && src.tv) {
-      url = sourceName === 'videasy' && imdb_id ? src.tv(imdb_id, season, episode) : src.tv(id, season, episode);
+    if (type === "movie" && src.movie) {
+      return imdb_id && source === "videasy" ? src.movie(imdb_id) : src.movie(id);
     }
-
-    if (url) {
-      sources.push({ source: sourceName, url });
+    if (type === "tv" && src.tv) {
+      return imdb_id && source === "videasy" ? src.tv(imdb_id, season, episode) : src.tv(id, season, episode);
+    }
+    if (type === "anime" && mal_id && src.anime) {
+      return src.anime(mal_id, episode || 1, "sub");
     }
   }
-
-  return sources;
-};
-
-export const getFirstEmbedUrl = (...args) => {
-  const list = getEmbedUrl(...args);
-  return list.length > 0 ? list[0] : null;
+  return null;
 };
